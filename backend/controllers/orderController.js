57dbfa0539b7743d8e411818b6f926e4955ab0b0
GauriKansal6533 <gaurikansal6533@gmail.com>
2025-09-12 00:01:23 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Place Order Controller
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://rabbit-a-food-delivery-appfrontend.onrender.com";
   const userId = req.userId;
   const { items, amount, address } = req.body;
 
